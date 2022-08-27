ce4795a1fbf530e053767dfe2f9cccce0a00e1fd
Fernando <ferweb.reyna@gmail.com>
2022-08-27 18:34:15 -0300

    utilizandoStyled

--- miapp/src/App.js
+++ miapp/src/App.js
@@ -4,10 +4,12 @@ import Destacados from './assets/Destacados';
 import Cupones from './assets/Cupones';
 import PromosApp from './assets/PromosApp';
 import Footer from './assets/Footer';
+import { NavBar } from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className='container-fluid fondo_amarillo'>
+      <NavBar/>
       <Header />
       <hr />
       <Destacados/>
@@ -19,3 +21,7 @@ function App() {
 }
 
 export default App;
+
+
+//COMPONENTS --- CONTAINERS
+
